8f05559cb054f05e7ae80aa9cd0a75b7e0f41054
rafacarrenho <rafael.carrenho@hotmail.com>
2021-05-27 20:56:18 -0300

    Feat: Home Page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,33 @@
+import { makeStyles, Typography } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  homeMain: {
+    width: "100%",
+    height: "100%",
+    display: "flex",
+    flexDirection: "column",
+    alignContent: "center",
+    justifyContent: "center",
+    textAlign: "center",
+  },
+  title: {
+    fontWeight: 700,
+    marginBottom: 40,
+  },
+}));
+
 export const Home = () => {
-  return <div>Home</div>;
+  const classes = useStyles();
+  return (
+    <div className={classes.homeMain}>
+      <Typography variant="h3" component="h1" className={classes.title}>
+        Bem vindo à<br />
+        Fractal Engenharia
+      </Typography>
+      <p>
+        Para visualizar os <b>gráficos</b> ou <b>mapas</b> utilize o menu
+        lateral.
+      </p>
+    </div>
+  );
 };
